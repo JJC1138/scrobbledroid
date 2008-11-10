9f65c46126b533a0944f76b4df98b4314f8d632c
jjc1138 <jjc1138@7ade96e0-b1d0-11dd-8714-116ca565c52f>
2008-11-10 02:59:25 +0000

    Added default application files from Eclipse ADT. Set svn:ignore for bin directory and R.java.

---
+++ src/net/jjc1138/android/scrobbler/ScrobblerConfig.java
@@ -0,0 +1,13 @@
+package net.jjc1138.android.scrobbler;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class ScrobblerConfig extends Activity {
+    /** Called when the activity is first created. */
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.main);
+    }
+}
