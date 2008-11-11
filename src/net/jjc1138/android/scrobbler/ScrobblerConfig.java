de1c6cb6e1f32c46c672618874188a7c45f94482
jjc1138 <jjc1138@7ade96e0-b1d0-11dd-8714-116ca565c52f>
2008-11-11 12:41:00 +0000

    Made the UI save and restore its state in line with the Activity life-cycle. Added save/revert buttons to UI. Updated a UI string. Renamed Ideas.txt to TODO.txt.

--- src/net/jjc1138/android/scrobbler/ScrobblerConfig.java
+++ src/net/jjc1138/android/scrobbler/ScrobblerConfig.java
@@ -1,13 +1,85 @@
 package net.jjc1138.android.scrobbler;
 
 import android.app.Activity;
+import android.content.SharedPreferences;
 import android.os.Bundle;
+import android.widget.Checkable;
+import android.widget.TextView;
 
 public class ScrobblerConfig extends Activity {
-    /** Called when the activity is first created. */
-    @Override
-    public void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.main);
-    }
+	SharedPreferences prefs;
+	SharedPreferences unsaved;
+
+	boolean settingsChanged;
+	
+	@Override
+	public void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		prefs = getSharedPreferences("prefs", 0);
+		unsaved = getSharedPreferences("unsaved", 0);
+		settingsChanged = false;
+		
+		setContentView(R.layout.main);
+	}
+
+	private void uiToPrefs(SharedPreferences p) {
+		SharedPreferences.Editor e = p.edit();
+		e.putBoolean("enable",
+			((Checkable) findViewById(R.id.enable)).isChecked());
+		e.putBoolean("immediate",
+			((Checkable) findViewById(R.id.immediate)).isChecked());
+		e.putString("username",
+			((TextView) findViewById(R.id.username)).getText().toString());
+		e.putString("password",
+			((TextView) findViewById(R.id.password)).getText().toString());
+		e.commit();
+	}
+
+	private void prefsToUI(SharedPreferences p) {
+		((Checkable) findViewById(R.id.enable)).setChecked(
+			p.getBoolean("enable", false));
+		((Checkable) findViewById(R.id.immediate)).setChecked(
+			p.getBoolean("immediate", false));
+		((TextView) findViewById(R.id.username)).setText(
+			p.getString("username", ""));
+		((TextView) findViewById(R.id.password)).setText(
+			p.getString("password", ""));
+	}
+
+	private boolean isUISaved() {
+		SharedPreferences p = prefs;
+		return
+			((Checkable) findViewById(R.id.enable)).isChecked() ==
+				p.getBoolean("enable", false) &&
+			((Checkable) findViewById(R.id.immediate)).isChecked() ==
+				p.getBoolean("immediate", false) &&
+			((TextView) findViewById(R.id.username)).getText().toString() ==
+				p.getString("username", "") &&
+			((TextView) findViewById(R.id.password)).getText().toString() ==
+				p.getString("password", "");
+	}
+
+	@Override
+	protected void onPause() {
+		super.onPause();
+		uiToPrefs(unsaved);
+	}
+
+	@Override
+	protected void onResume() {
+		super.onResume();
+		
+		final String examplePref = "enable";
+		if (unsaved.contains(examplePref)) {
+			prefsToUI(unsaved);
+			SharedPreferences.Editor e = unsaved.edit();
+			e.clear();
+			e.commit();
+		} else if (prefs.contains(examplePref)) {
+			prefsToUI(prefs);
+		} else {
+			// Store defaults:
+			uiToPrefs(prefs);
+		}
+	}
 }
