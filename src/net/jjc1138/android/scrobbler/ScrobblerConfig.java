2ae7b719ed8480e3837ecfb267a6db6a6721df0a
jjc1138 <jjc1138@7ade96e0-b1d0-11dd-8714-116ca565c52f>
2008-11-11 13:45:45 +0000

    Added saving of preferences. Used member variables for the view elements rather than using findViewById repeatedly. Added design doc.

--- src/net/jjc1138/android/scrobbler/ScrobblerConfig.java
+++ src/net/jjc1138/android/scrobbler/ScrobblerConfig.java
@@ -3,66 +3,118 @@ package net.jjc1138.android.scrobbler;
 import android.app.Activity;
 import android.content.SharedPreferences;
 import android.os.Bundle;
-import android.widget.Checkable;
-import android.widget.TextView;
+import android.text.Editable;
+import android.text.TextWatcher;
+import android.view.View;
+import android.widget.Button;
+import android.widget.CheckBox;
+import android.widget.CompoundButton;
+import android.widget.EditText;
+import android.widget.LinearLayout;
 
 public class ScrobblerConfig extends Activity {
 	SharedPreferences prefs;
 	SharedPreferences unsaved;
 
-	boolean settingsChanged;
+	CheckBox enable;
+	CheckBox immediate;
+	EditText username;
+	EditText password;
+
+	LinearLayout settingsChanged;
 	
 	@Override
 	public void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		prefs = getSharedPreferences("prefs", 0);
 		unsaved = getSharedPreferences("unsaved", 0);
-		settingsChanged = false;
+		CompoundButton.OnCheckedChangeListener checkWatcher =
+			new CompoundButton.OnCheckedChangeListener() {
+			
+			@Override
+			public void onCheckedChanged(CompoundButton buttonView,
+					boolean isChecked) {
+				settingsChanged();
+			}
+		};
+		TextWatcher textWatcher = new TextWatcher() {
+			@Override
+			public void afterTextChanged(Editable s) {
+				settingsChanged();
+			}
+			@Override
+			public void beforeTextChanged(CharSequence s, int start, int count,
+					int after) {}
+			@Override
+			public void onTextChanged(CharSequence s, int start, int before,
+					int count) {}
+		};
 		
 		setContentView(R.layout.main);
+		enable = (CheckBox) findViewById(R.id.enable);
+		immediate = (CheckBox) findViewById(R.id.immediate);
+		username = (EditText) findViewById(R.id.username);
+		password = (EditText) findViewById(R.id.password);
+
+		settingsChanged = (LinearLayout) findViewById(R.id.settings_changed);
+		
+		enable.setOnCheckedChangeListener(checkWatcher);
+		immediate.setOnCheckedChangeListener(checkWatcher);
+		username.addTextChangedListener(textWatcher);
+		password.addTextChangedListener(textWatcher);
+		((Button) findViewById(R.id.save)).setOnClickListener(
+			new View.OnClickListener() {
+				@Override
+				public void onClick(View v) {
+					uiToPrefs(prefs);
+					settingsChanged.setVisibility(View.GONE);
+				}
+			});
+		((Button) findViewById(R.id.revert)).setOnClickListener(
+			new View.OnClickListener() {
+				@Override
+				public void onClick(View v) {
+					prefsToUI(prefs);
+					settingsChanged.setVisibility(View.GONE);
+				}
+			});
+	}
+
+	protected void settingsChanged() {
+		settingsChanged.setVisibility(isUISaved() ? View.GONE : View.VISIBLE);
 	}
 
 	private void uiToPrefs(SharedPreferences p) {
 		SharedPreferences.Editor e = p.edit();
-		e.putBoolean("enable",
-			((Checkable) findViewById(R.id.enable)).isChecked());
-		e.putBoolean("immediate",
-			((Checkable) findViewById(R.id.immediate)).isChecked());
-		e.putString("username",
-			((TextView) findViewById(R.id.username)).getText().toString());
-		e.putString("password",
-			((TextView) findViewById(R.id.password)).getText().toString());
+		e.putBoolean("enable", enable.isChecked());
+		e.putBoolean("immediate", immediate.isChecked());
+		e.putString("username", username.getText().toString());
+		e.putString("password", password.getText().toString());
 		e.commit();
 	}
 
 	private void prefsToUI(SharedPreferences p) {
-		((Checkable) findViewById(R.id.enable)).setChecked(
-			p.getBoolean("enable", false));
-		((Checkable) findViewById(R.id.immediate)).setChecked(
-			p.getBoolean("immediate", false));
-		((TextView) findViewById(R.id.username)).setText(
-			p.getString("username", ""));
-		((TextView) findViewById(R.id.password)).setText(
-			p.getString("password", ""));
+		enable.setChecked(p.getBoolean("enable", false));
+		immediate.setChecked(p.getBoolean("immediate", false));
+		username.setText(p.getString("username", ""));
+		password.setText(p.getString("password", ""));
 	}
 
 	private boolean isUISaved() {
 		SharedPreferences p = prefs;
 		return
-			((Checkable) findViewById(R.id.enable)).isChecked() ==
-				p.getBoolean("enable", false) &&
-			((Checkable) findViewById(R.id.immediate)).isChecked() ==
-				p.getBoolean("immediate", false) &&
-			((TextView) findViewById(R.id.username)).getText().toString() ==
-				p.getString("username", "") &&
-			((TextView) findViewById(R.id.password)).getText().toString() ==
-				p.getString("password", "");
+			enable.isChecked() == p.getBoolean("enable", false) &&
+			immediate.isChecked() == p.getBoolean("immediate", false) &&
+			username.getText().toString().equals(p.getString("username", "")) &&
+			password.getText().toString().equals(p.getString("password", ""));
 	}
 
 	@Override
 	protected void onPause() {
 		super.onPause();
-		uiToPrefs(unsaved);
+		if (settingsChanged.getVisibility() == View.VISIBLE) {
+			uiToPrefs(unsaved);
+		}
 	}
 
 	@Override
@@ -75,8 +127,11 @@ public class ScrobblerConfig extends Activity {
 			SharedPreferences.Editor e = unsaved.edit();
 			e.clear();
 			e.commit();
+			settingsChanged.setVisibility(View.VISIBLE);
 		} else if (prefs.contains(examplePref)) {
 			prefsToUI(prefs);
+			// Reset this because it is set by prefsToUI changing things:
+			settingsChanged.setVisibility(View.GONE);
 		} else {
 			// Store defaults:
 			uiToPrefs(prefs);
