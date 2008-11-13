0a471d79255b9ab390616a6b435d4c593eae7517
jjc1138 <jjc1138@7ade96e0-b1d0-11dd-8714-116ca565c52f>
2008-11-13 00:21:25 +0000

    Added first stubby version of MusicBroadcastReceiver. Added "Android Bugs.txt" doc.

---
+++ src/net/jjc1138/android/scrobbler/MusicBroadcastReceiver.java
@@ -0,0 +1,16 @@
+package net.jjc1138.android.scrobbler;
+
+import android.content.BroadcastReceiver;
+import android.content.Context;
+import android.content.Intent;
+import android.util.Log;
+
+public class MusicBroadcastReceiver extends BroadcastReceiver {
+
+	@Override
+	public void onReceive(Context context, Intent intent) {
+		Log.v("Scrob", intent.toString());
+		Log.v("ScrobE", intent.getExtras().keySet().toString());
+	}
+
+}
